e1528cdcf2ae4fa75a8cfdd3668935b6e35c5b66
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-19 14:53:48 +0300

    refactor server.ts to use environment configuration and connect to the database

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express from "express";
+import { config } from "./config/env.config";
+import { connect } from "mongoose";
+
+const { port, host } = config.server;
+const { connectionString } = config.database;
 
 const app = express();
 
-const PORT = 8000;
-
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+connect(connectionString).then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+  });
 });

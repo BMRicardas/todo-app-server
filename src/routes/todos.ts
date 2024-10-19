63eef0a6bb232d97f42b4460549201616a0f4b4c
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-19 15:37:41 +0300

    refactor todo routes to use controller functions

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,9 +1,22 @@
 import { Router } from "express";
+import {
+  createTodo,
+  deleteTodo,
+  getTodo,
+  getTodos,
+  updateTodo,
+} from "../controllers/todos";
 
 const routes = Router();
 
-routes.get("/todos", (req, res) => {
-  res.send("Hello Todos");
-});
+routes.get("/todos", getTodos);
+
+routes.get("/todos/:id", getTodo);
+
+routes.post("/todos", createTodo);
+
+routes.put("/todos/:id", updateTodo);
+
+routes.delete("/todos/:id", deleteTodo);
 
 export default routes;

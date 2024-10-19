600ad750b41e09810f8a3202790cf94ca1f8b708
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-19 15:37:50 +0300

    refactor server.ts to use environment configuration and connect to the database

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,31 @@
-import express from "express";
-import { config } from "./config/env.config";
+import express, { json } from "express";
 import { connect } from "mongoose";
+
+import { config } from "./config/env.config";
+import todoRoutes from "./routes/todos";
 
 const { port, host } = config.server;
 const { connectionString } = config.database;
 
 const app = express();
 
-app.get("/", (_req, res) => {
-  res.send("Hello World");
-});
+app.use(json());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
-connect(connectionString).then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-  });
-});
+app.use(todoRoutes);
+
+connect(connectionString)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on http://${host}:${port}`);
+    });
+  })
+  .catch((error) => console.error(error));

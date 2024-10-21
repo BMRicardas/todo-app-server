8319b4336222db0d57e6e02ecfc83364e6e37396
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-21 20:30:52 +0300

    refactor package.json and package-lock.json to add cors dependency

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { connect } from "mongoose";
 
 import { config } from "./config/env.config";
 import todoRoutes from "./routes/todos";
+import cors from "cors";
 import helmet from "helmet";
 
 const { port, host } = config.server;
@@ -10,17 +11,10 @@ const { connectionString } = config.database;
 
 const app = express();
 
+app.use(cors());
+
 app.use(helmet());
 app.use(json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.use(todoRoutes);
 
